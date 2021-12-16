b5bd70bdeace79eb7ddcdeb36b9ee1bd35c969dd
Izik shtemer <izik.shtemer@gmail.com>
2021-12-17 01:34:12 +0200

    Add basic email example

--- Email/Email.cs
+++ Email/Email.cs
@@ -1,12 +1,49 @@
-﻿namespace Email
-{
-    public class Email
-    {
-        private bool SendEmail(string from,string to,string body)
-        {
-            bool results = false;
+﻿using System.Net;
+using System.Net.Mail;
 
-            return results;
+public class Email
+{
+    public static bool SendEmail(string from, string to, string subject, string body, string server, string emailUserName, string emailPassword)
+    {
+        bool results = false;
+
+        MailMessage message = new MailMessage(from, to);
+        message.Subject = subject;
+        message.Body = body;
+
+        #region Version 1
+        // If credentials are not necessary use this instead of Version 1 or 2
+        //client.UseDefaultCredentials = true;
+        #endregion
+
+        #region Version 2
+        SmtpClient client = new SmtpClient(server);
+        client.Port = 587;
+        client.Credentials = new NetworkCredential(emailUserName, emailPassword);
+        client.EnableSsl = true;
+        #endregion
+
+        #region Version 3
+        //var smtpClient = new SmtpClient(server)
+        //{
+        //    Port = 587,
+        //    Credentials = new NetworkCredential(emailUserName, emailPassword),
+        //    EnableSsl = true,
+        //};
+        #endregion
+
+
+
+        try
+        {
+            client.Send(message);
+            results = true;
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception caught in CreateTestMessage2(): {0}", ex.ToString());
+        }
+
+        return results;
     }
 }
---
+++ EmailExample/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string from = "myEmail@gmail.com";
+string to = "izik.shtemer@gmail.com";
+string subject = "Hello mike";
+string body = "Hello world";
+string mailServer = "smtp.gmail.com";
+string emailUserName = "emailUserName";
+string emailPassword = "emailPassword";
+
+bool results = Email.SendEmail(from, to,subject, body,mailServer,emailUserName,emailPassword);
+
