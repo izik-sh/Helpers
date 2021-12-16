8978baf99563d38566713847600d72c9fb1b3301
Izik shtemer <izik.shtemer@gmail.com>
2021-12-17 01:09:24 +0200

    add class
    add

--- Email/Email.cs
+++ Email/Email.cs
@@ -2,6 +2,11 @@
 {
     public class Email
     {
+        private bool SendEmail(string from,string to,string body)
+        {
+            bool results = false;
 
+            return results;
+        }
     }
 }
