e35f2667af51ace45d38bcecaa8b3d0880d6b2d1
izik sh <izik.shtemer@gmail.com>
2022-02-17 23:31:01 +0200

    Add windows service that shutdown the computer.

---
+++ WindowsService/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            if (Environment.UserInteractive)
+            {
+                Service service = new Service();
+                service.RunTasks(null,null);
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
+                new Service()
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+    }
+}
---
+++ WindowsService/Service.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsService
+{
+    public partial class Service : ServiceBase
+    {
+        #region Params
+        string timeToShutDown = "21:00:00";
+        int timerIntervalInSeconds = 60000;
+        #endregion
+
+        public Service()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            timeToShutDown = ConfigurationManager.AppSettings["TimeToShutDown"];
+            timerIntervalInSeconds = Convert.ToInt32(ConfigurationManager.AppSettings["TimerIntervalInSeconds"]) * 1000;
+
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            System.Timers.Timer T1 = new System.Timers.Timer();
+            T1.Interval = (timerIntervalInSeconds);
+            T1.AutoReset = true;
+            T1.Enabled = true;
+            T1.Start();
+            T1.Elapsed += new System.Timers.ElapsedEventHandler(RunTasks);
+        }
+
+        public void RunTasks(object sender, ElapsedEventArgs e)
+        {
+            string[] time = timeToShutDown.Split(':');
+
+            int results = TimeSpan.Compare(DateTime.Now.TimeOfDay, new TimeSpan(Convert.ToInt32(time[0]), Convert.ToInt32(time[1]), Convert.ToInt32(time[2])));
+
+            if (results == 1)
+            {
+                ComputerShutDown.ShutDown();
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
